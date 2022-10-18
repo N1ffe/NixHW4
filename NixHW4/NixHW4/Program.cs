f4a42449076524622ad01b7e5789101d9e776d75
Niffe <petrovnik01@ukr.net>
2022-10-18 19:04:18 +0300

    Create project

---
+++ NixHW4/NixHW4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NixHW4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
